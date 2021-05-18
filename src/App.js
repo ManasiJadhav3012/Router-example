1811e8403529ba16dedbf6115591d27b015304f4
unknown <jadhavmanasi00@gmail.com>
2021-05-18 12:36:49 +0530

    Just Trying how routing works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+
+const HomePage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <Link to='/topics'>Topics</Link>
+      <br/>
+      <button onClick={() => props.history.push('/topics')}> Topics </button>
+      <h1>HOME PAGE</h1>
+    </div>
+  )
+}
+
+const TopicsList = (props) => {
+  return (
+    <div>
+      <h1>TOPIC LIST PAGE</h1>
+      <Link to={`${props.match.url}/:13`}>To Topic 13</Link>
+      <br/>
+      <Link to={`${props.match.url}/:17`}>To Topic 17</Link>
+      <br />
+      <Link to={`${props.match.url}/:21`}>To Topic 21</Link>
+      <br />
+    </div>
+  )
+}
+
+const TopicDetail = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>TOPIC DETAIL PAGE: { props.match.params.topicId }</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Route exact path='/' component={HomePage}></Route>
+      <Route exact path='/topics' component={TopicsList}></Route>
+      <Route path='/topics/:topicId' component={TopicDetail}></Route>
+
+      <Route exact path='/blog/topics' component={TopicsList}></Route>
+      <Route path='/blog/topics/:topicId' component={TopicDetail}></Route>
+
     </div>
   );
 }
